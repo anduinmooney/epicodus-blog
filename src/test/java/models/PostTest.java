b3388123a400e3ddf46bf7aebcbefd93285a9a91
Anduin Mooney <anduinmooney@gmail.com>
2018-01-09 11:28:23 -0800

    Create site.

---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,39 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 1/9/18.
+ */
+public class PostTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
+    }
+
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+    }
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertTrue(Post.getAll().contains(post));
+        assertTrue(Post.getAll().contains(otherPost));
+    }
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
