edecb1cb800adf8941a60c17df3e467ad2e79cb0
Anduin Mooney <anduinmooney@gmail.com>
2018-01-10 11:12:20 -0800

    Update blog site.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,38 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import models.Post;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+import sun.jvm.hotspot.oops.ObjectHeap;
+
+import javax.print.DocFlavor;
+
 import static spark.Spark.*;
+
 
 public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocreate this
         staticFileLocation("/public");
 
+        //get: show new post form
+        get("/posts/new", (req, res) ->  {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "post-form.hbs");
+        }, new HandlebarsTemplateEngine());
 
+        //post: process new post form
+        post("/posts/new", (req, res) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<>();
+            String content = req.queryParams("content");
+            Post newPost = new Post(content);
+            model.put("post", newPost);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
 
+        //get: show all posts
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             ArrayList<Post> posts = Post.getAll();
@@ -22,18 +41,46 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-
-        //route that makes new post
-        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+        //get: delete all posts
+        get("/posts/delete", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-
-            String content = request.queryParams("content");
-            Post newPost = new Post(content);
-            model.put("post", newPost);
+            Post.clearAllPosts();
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //get: show an individual post
+        get("/posts/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Post foundPost = Post.findById(idOfPostToFind); //use it to find post
+            model.put("post", foundPost); //add it to model for template to display
+            return new ModelAndView(model, "post-detail.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
+        //get: show a form to update a post
+        get("/posts/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToEdit = Integer.parseInt(req.params("id"));
+            Post editPost = Post.findById(idOfPostToEdit);
+            model.put("editPost", editPost);
+            return new ModelAndView(model, "post-form.hbs");
+        }, new HandlebarsTemplateEngine());
 
-
+        //post: process a form to update a post
+        post("/posts/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String newContent = req.queryParams("content");
+            int idOfPostToEdit = Integer.parseInt(req.params("id"));
+            Post editPost = Post.findById(idOfPostToEdit);
+            editPost.update(newContent); //don’t forget me
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        //get: delete an individual post
+        get("posts/:id/delete", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToDelete = Integer.parseInt(request.params("id"));
+            Post deletePost = Post.findById(idOfPostToDelete);
+            deletePost.deletePost();;
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,30 +1,53 @@
 package models;
-
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-/**
- * Created by staff on 7/10/17.
- */
 public class Post {
-
-    private final String content;
+    private String content;
     private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
+    private int id;
 
     public Post (String content){
         this.content = content;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
         instances.add(this);
+        this.id = instances.size();
+
+    }
+
+    public static Post findById(int id){
+        return  instances.get(id-1);
     }
 
     public String getContent() {
+
         return content;
     }
-
 
     public static ArrayList<Post> getAll(){
         return instances;
     }
 
-    public static void clearAllPosts(){
+    public static void clearAllPosts() {
         instances.clear();
+    }
+    public boolean getPublished(){
+        return this.published;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+    public void deletePost() {
+        instances.remove(id-1);
     }
 }
